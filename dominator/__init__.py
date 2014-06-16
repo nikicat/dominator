34f66ab99e16fe36d71d6490cfa3d75782b32c3c
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-06-16 18:12:48 +0400

    __init__.py: export Image

--- dominator/__init__.py
+++ dominator/__init__.py
@@ -1,11 +1,11 @@
 from pkg_resources import get_distribution, DistributionNotFound
-from .entities import Ship, LocalShip, Container, DataVolume, ConfigVolume, TextFile, TemplateFile, YamlFile
+from .entities import Ship, LocalShip, Image, Container, DataVolume, ConfigVolume, TextFile, TemplateFile, YamlFile
 from .utils import ships_from_conductor, ships_from_nova, datacenter_from_racktables, aslist, asdict, groupby, \
     get_image, image_ports, cached
 from .__main__ import main  # NOQA: this is needed for dominator script to work
 
-__all__ = ['Ship', 'LocalShip', 'Container', 'DataVolume', 'ConfigVolume',
-            'TextFile', 'TemplateFile', 'YamlFile',
+__all__ = ['Ship', 'LocalShip', 'Image', 'Container', 'DataVolume', 'ConfigVolume',
+           'TextFile', 'TemplateFile', 'YamlFile',
            'ships_from_conductor', 'ships_from_nova', 'datacenter_from_racktables',
            'aslist', 'asdict', 'groupby', 'cached',
            'get_image', 'image_ports']

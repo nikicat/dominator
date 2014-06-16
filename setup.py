fd45f6a5e7cddec1401fe67c5cc37a157d042648
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-06-16 18:13:44 +0400

    added tests

--- setup.py
+++ setup.py
@@ -1,4 +1,26 @@
+import sys
 import setuptools
+from setuptools.command.test import test as TestCommand
+
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import tox
+        import shlex
+        errno = tox.cmdline(args=shlex.split(self.tox_args))
+        sys.exit(errno)
+
 
 if __name__ == '__main__':
     setuptools.setup(
@@ -33,4 +55,11 @@ if __name__ == '__main__':
             'conductor_client',
             'psutil',
         ],
+        tests_require=[
+            'tox',
+            'coveralls',
+            'pytest',
+            'pytest-cov',
+        ],
+        cmdclass={'test': Tox},
     )
---
+++ test/test_actions.py
@@ -0,0 +1,66 @@
+import tempfile
+import logging
+import re
+import pytest
+import vcr
+from colorama import Fore
+
+from dominator.entities import LocalShip, Container, Image
+from dominator.actions import dump, run, status, initlog, load_yaml
+from dominator.settings import settings as _settings
+
+
+@pytest.yield_fixture(autouse=True)
+def settings():
+    with tempfile.TemporaryDirectory() as configdir:
+        _settings['configvolumedir'] = configdir
+        yield _settings
+
+
+@pytest.fixture(autouse=True)
+def logs():
+    initlog()
+    logging.basicConfig(level=logging.DEBUG)
+
+
+@pytest.fixture
+def ships():
+    return [LocalShip()]
+
+
+@pytest.fixture
+def containers():
+    return [Container(
+            name='testcont',
+            ship=ship,
+            image=Image('busybox'),
+            command='sleep 10') for ship in ships()]
+
+
+@pytest.fixture
+def docker():
+    import docker
+    return docker.Client()
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/run.yaml')
+def test_run(capsys, containers):
+    # FIXME: use https endpoint because vcrpy doesn't handle UnixHTTPConnection
+    run(containers, dockerurl='http://localhost:4243')
+    _, _ = capsys.readouterr()
+    status(containers)
+    out, _ = capsys.readouterr()
+    assert re.match(r'  testcont[ \t]+Up Less than a second[ \t]+{color}{id:.7}'.format(
+        color=re.escape(Fore.GREEN), id=containers[0].image.id), out.split('\n')[-2])
+
+
+def test_dump(capsys, containers):
+    dump(containers)
+    dump1, _ = capsys.readouterr()
+    assert dump1 != ''
+    with tempfile.NamedTemporaryFile(mode='w') as tmp:
+        tmp.write(dump1)
+        tmp.flush()
+        dump(load_yaml(tmp.name))
+    dump2, _ = capsys.readouterr()
+    assert dump1 == dump2

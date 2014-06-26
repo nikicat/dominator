a0c341a560e24006c8b1bce70e237e7a5c8783dd
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-06-26 16:24:52 +0400

    version changed to 1.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class PyTest(TestCommand):
 if __name__ == '__main__':
     setuptools.setup(
         name='dominator',
-        version='0.4',
+        version='1.0',
         url='https://github.com/yandex-sysmon/dominator',
         license='GPLv3',
         author='Nikolay Bryskin',

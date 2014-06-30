602608e08f8daef34ef5a31dcb88b713e8474511
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-07-01 02:45:21 +0400

    version bump to 2.0 due to api incompatibilities

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class PyTest(TestCommand):
 if __name__ == '__main__':
     setuptools.setup(
         name='dominator',
-        version='1.0',
+        version='2.0',
         url='https://github.com/yandex-sysmon/dominator',
         license='GPLv3',
         author='Nikolay Bryskin',

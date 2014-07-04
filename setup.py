c0f04c7eb6450824c5a4c9178a749d6029d617df
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-07-05 03:07:08 +0400

    bump version to 3.0 due to api change (volumes.files becomes a dict)

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class PyTest(TestCommand):
 if __name__ == '__main__':
     setuptools.setup(
         name='dominator',
-        version='2.1',
+        version='3.0',
         url='https://github.com/yandex-sysmon/dominator',
         license='GPLv3',
         author='Nikolay Bryskin',

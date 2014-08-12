5b6d0b2b605b8f1e3465f394dac0b50213f40e17
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-08-12 18:56:19 +0400

    Bump version: 6.0 → 6.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class PyTest(TestCommand):
 if __name__ == '__main__':
     setuptools.setup(
         name='dominator',
-        version='6.0',
+        version='6.1',
         url='https://github.com/yandex-sysmon/dominator',
         license='LGPLv3',
         author='Nikolay Bryskin',

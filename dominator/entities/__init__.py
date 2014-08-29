78573738754f7e16a3d63c865891f405bbf06759
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-08-29 12:51:42 +0400

    Ship: allow to customize Docker port

--- dominator/entities/__init__.py
+++ dominator/entities/__init__.py
@@ -46,9 +46,10 @@ class Ship(BaseShip):
     """
     Ship objects represents host running Docker listening on 4243 external port.
     """
-    def __init__(self, name, fqdn, **kwargs):
+    def __init__(self, name, fqdn, port=2375, **kwargs):
         self.name = name
         self.fqdn = fqdn
+        self.port = port
         for k, v in kwargs.items():
             setattr(self, k, v)
 
@@ -61,7 +62,7 @@ class Ship(BaseShip):
     @utils.cached
     def docker(self):
         self.logger.debug('connecting to ship', fqdn=self.fqdn)
-        return docker.Client('http://{}:2375'.format(self.fqdn))
+        return docker.Client('http://{}:{}'.format(self.fqdn, self.port))
 
 
 class LocalShip(BaseShip):

ea07911dca77ffcd26bbd471b73a379a43e57e56
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-10-07 03:27:27 +0400

    pinned urllib3 version (>=1.9.1)

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
         ],
         extras_require={
             'full': ['PyYAML.Yandex >= 3.11.1', 'colorlog', 'requests_cache', 'tzlocal', 'pkginfo', 'openssh_wrapper',
-                     'objgraph', 'pyopenssl', 'psutil', 'vcrpy', 'requests>=2.4'],
+                     'objgraph', 'pyopenssl', 'psutil', 'vcrpy', 'requests>=2.4', 'urllib3>=1.9.1'],
             'tiny': ['PyYAML'],
         },
         cmdclass={'test': PyTest},

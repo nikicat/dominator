58dc0564270462eb6fb01724e29668db86f898d3
Nikolay Bryskin <nbryskin@yandex-team.ru>
2014-11-14 02:09:05 +0300

    Bump version: 13.2.0 → 14.0.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class PyTest(TestCommand):
 if __name__ == '__main__':
     setuptools.setup(
         name='dominator',
-        version='13.2.0',
+        version='14.0.0',
         url='https://github.com/yandex-sysmon/dominator',
         license='LGPLv3',
         author='Nikolay Bryskin',
